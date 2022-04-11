e50cd3042b4a2200c61d0d5c33d79103909797a0
unknown <moh.afifi93@gmail.com>
2022-04-11 22:14:23 +0200

    9:28

--- udagram/udagram-frontend/src/karma.conf.js
+++ udagram/udagram-frontend/src/karma.conf.js
@@ -10,6 +10,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
+
       require('karma-chrome-launcher'),
       require('@chiragrupani/karma-chromium-edge-launcher'),
       require('karma-firefox-launcher'),
