d02093f46eed2a2d8bd2a124d2c796c3ab353c72
unknown <moh.afifi93@gmail.com>
2022-04-11 22:43:13 +0200

    10:34

--- udagram/udagram-frontend/src/karma.conf.js
+++ udagram/udagram-frontend/src/karma.conf.js
@@ -29,7 +29,6 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: false,
     browsers: ['ChromeHeadlessNoSandbox'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
