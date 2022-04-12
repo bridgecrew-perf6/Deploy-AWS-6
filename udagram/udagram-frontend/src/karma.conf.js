1731fdedfbbf3aea9187718d2daa5380ac62b286
unknown <moh.afifi93@gmail.com>
2022-04-12 21:02:57 +0200

    10:34

--- udagram/udagram-frontend/src/karma.conf.js
+++ udagram/udagram-frontend/src/karma.conf.js
@@ -20,6 +20,7 @@ module.exports = function (config) {
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage'),
       reports: ['html', 'lcovonly', 'text-summary'],
